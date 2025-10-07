0f13a2dbd9083ecbd984aaddf9325925cd09ab7b
Gia Bảo <NgoBao@MacBook-Pro-cua-Bao.local>
2025-10-07 11:59:37 +0700

    Ura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Iridescence from "@/components/layout/background/Iridescence";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="h-screen w-screen bg-black">
+      <Iridescence
+        color={[1, 1, 1]}
+        mouseReact={false}
+        amplitude={0.1}
+        speed={1.0}
+      />
+    </div>
+  );
 };
 
 export default page;
--- src/components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { PageHeader } from "./headers/PageHeader";
-export { StatsCards, type StatItem } from "./stats/StatsCards";
-export { SearchAndFilter, type FilterOption } from "./filters/SearchAndFilter";
-export { ActionButtons, type ActionButton } from "./actions/ActionButtons";
-export { DataTable } from "./table/DataTable";
